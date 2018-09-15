141a5fe3f0256436901c8ce44951554bdea0d3c2
mezubeen <zubeen95@gmail.com>
2018-09-16 03:16:11 +0530

    home page completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    NotFoundComponent,
+    PortfolioComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+     {
+        path: '', 
+        component: HomeComponent
+     },
+     {
+         path: 'portfolio', 
+         component: PortfolioComponent 
+     },
+    {
+        path: 'contact', 
+        component: ContactComponent 
+     },
+      {
+        path: '**', 
+        component: NotFoundComponent 
+     }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
