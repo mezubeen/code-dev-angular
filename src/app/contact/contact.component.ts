25e5f1e56b1b3fc114ed5c31ac4174a0ab85bb8d
mezubeen <zubeen95@gmail.com>
2018-09-20 04:03:15 +0530

    dynamically adding js files on each comp. with life-scyle hooks

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,12 @@
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
   data$: FirebaseListObservable<any[]>;
   submitted: boolean = false;
   // submittedHeading: true;
@@ -23,6 +23,30 @@ export class ContactComponent {
     this.submitted = true;
     
 
+  }
+
+  ngOnInit() {
+    // this.loadScript('http://www.some-library.com/library.js');
+
+    this.loadScript('../assets/js/modernizr-2.6.2.min.js');
+    this.loadScript('../assets/js/jquery.min.js');
+    this.loadScript('../assets/js/jquery.easing.1.3.js');
+    this.loadScript('../assets/js/bootstrap.min.js');
+    this.loadScript('../assets/js/jquery.waypoints.min.js');
+    this.loadScript('../assets/js/jquery.flexslider-min.js');
+    this.loadScript('../assets/js/main.js');
+    this.loadScript('../assets/js/sticky-kit.min.js');
+  }
+
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
   }
 
 
--- src/app/enquiries/enquiries.component.ts
+++ src/app/enquiries/enquiries.component.ts
@@ -18,8 +18,27 @@ export class EnquiriesComponent implements OnInit {
         
       })
   }
-
   ngOnInit() {
+    // this.loadScript('http://www.some-library.com/library.js');
+
+    this.loadScript('../assets/js/modernizr-2.6.2.min.js');
+    this.loadScript('../assets/js/jquery.min.js');
+    this.loadScript('../assets/js/jquery.easing.1.3.js');
+    this.loadScript('../assets/js/bootstrap.min.js');
+    this.loadScript('../assets/js/jquery.waypoints.min.js');
+    this.loadScript('../assets/js/jquery.flexslider-min.js');
+    this.loadScript('../assets/js/main.js');
+    this.loadScript('../assets/js/sticky-kit.min.js');
   }
 
+
+  public loadScript(url: string) {
+    const body = <HTMLDivElement> document.body;
+    const script = document.createElement('script');
+    script.innerHTML = '';
+    script.src = url;
+    script.async = false;
+    script.defer = true;
+    body.appendChild(script);
+  }
 }
