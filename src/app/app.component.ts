9f818d9eb9c85fdc001b880c2cd84123634b7ef3
mezubeen <zubeen95@gmail.com>
2018-09-23 03:50:43 +0530

    scrolling issue fixed on component change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+    onActivate(event) {
+
+        if (event.constructor.name==="PortfolioComponent" || event.constructor.name==="ContactComponent"){
+
+            let scrollToTop = window.setInterval(() => {
+                let pos = window.pageYOffset;
+                if (pos > 0) {
+                    window.scrollTo(0, pos - 20);
+                } else {
+                    window.clearInterval(scrollToTop);
+                }
+            }, 16);
+
+        }
+
+
+    }
 }
